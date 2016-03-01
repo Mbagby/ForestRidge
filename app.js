cbe4a108589a11ff0a3d7fbb93b436c1dfb502d5
Madeline <madelinebagby@yahoo.com>
2016-03-01 10:42:13 -0800

    putting this online

---
+++ app.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$('#locationModal').click(function(){
+  	$('.ui.modal')
+  		.modal('show');
+  });
+  $('.slider1').bxSlider({
+    slideWidth: 300,
+    minSlides: 2,
+    maxSlides: 3,
+    slideMargin: 10
+  });
+
+  
+});
