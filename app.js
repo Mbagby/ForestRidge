a0003f0eec3b04087977c295338188b46e90dc57
Madeline <madelinebagby@yahoo.com>
2016-03-01 17:46:03 -0800

    css stuff

--- app.js
+++ app.js
@@ -12,3 +12,25 @@ $(document).ready(function(){
   $(".bx-controls-direction")[1].hide();
 
 });
+
+
+// var applicationFee= 40
+// var oneBedroom= 850
+// var twoBedroom= 1000
+// var dogMonthlyFee= 25
+// var catMonthlyFee= 15
+// var dogDeposit= 200
+// var catDeposit= 200
+// var dogFee= 200
+// var catFee= 200
+// var oneBedroomSecurity = 850
+// var twoBedroomSecurity = 1000
+
+
+// var oneTimeExpenses= function(){
+//   var oneTimeExpenses = 0;
+//    oneTimeExpenses += applicationFee
+//    if()
+// }
+
+// var monthlyExpenses =
